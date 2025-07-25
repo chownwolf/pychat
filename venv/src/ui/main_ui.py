240a51d4e1d25e92569d1b7f90c8f5eb147b657f
chownwolf <woodstockxx@gmail.com>
2025-07-25 15:18:48 -0400

    fix

--- venv/src/ui/main_ui.py
+++ venv/src/ui/main_ui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import scrolledtext, messagebox, simpledialog
+from tkinter import scrolledtext, messagebox
 from src.pychat import add_user_to_chat, remove_user_from_chat, send_message_to_chat
 from src.chat.chat_room import ChatRoom
 
